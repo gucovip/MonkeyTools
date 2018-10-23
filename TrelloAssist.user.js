6aec1a5637c3c5bfd5b4e8cf7bb42adca5fd38f6
Cong <raiyeeeric@gmail.com>
2018-10-23 14:25:01 +0800

    Update TrelloAssist.user.js

--- TrelloAssist.user.js
+++ TrelloAssist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trello Assist
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://trello.com/*
