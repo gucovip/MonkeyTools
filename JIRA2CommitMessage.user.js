a37a5b7f81e36ea8d5659bcc05b6428cb25e6ecc
ZiYuan <hanziyuan08@outlook.com>
2019-05-10 15:51:58 +0800

    reactive jira helper

--- JIRA2CommitMessage.user.js
+++ JIRA2CommitMessage.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JIRA2CommitMessage
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  try to take over the world!
 // @author       Cong, Ziyuan
 // @match        http://jira.carzone365.com/browse/*
@@ -13,17 +13,30 @@
 (function() {
     'use strict';
 
-    $(document.querySelector('#stalker > div > div > div > div > div.toolbar-split.toolbar-split-left')).append('<ul id="jira-helper" class="toolbar-group pluggable-ops"></ul>')
-    $(document.querySelector('#jira-helper')).append('<li class="toolbar-item"><a id="copy-simple" class="toolbar-trigger issueaction-workflow-transition">复制(简单)</a></li>')
-    $(document.querySelector('#jira-helper')).append('<li class="toolbar-item"><a id="copy-detailed" class="toolbar-trigger issueaction-workflow-transition">复制(详细)</a></li>')
+    function addJiraHelper() {
+        const toolbar = document.querySelector('.toolbar-split.toolbar-split-left')
+        $(toolbar).append('<ul id="jira-helper" class="toolbar-group pluggable-ops"></ul>')
+        $(document.querySelector('#jira-helper')).append('<li class="toolbar-item"><a id="copy-simple" class="toolbar-trigger issueaction-workflow-transition">复制(简单)</a></li>')
+        $(document.querySelector('#jira-helper')).append('<li class="toolbar-item"><a id="copy-detailed" class="toolbar-trigger issueaction-workflow-transition">复制(详细)</a></li>')
 
-    $('#copy-simple').on('click', function(){
-        const jiraCode = $('.aui-nav.aui-nav-breadcrumbs').children(':last-child').text()
-        GM_setClipboard(jiraCode, { type: 'text', mimetype: 'text/plain'})
-    })
-    $('#copy-detailed').on('click', function(){
-        const jiraCode = $('.aui-nav.aui-nav-breadcrumbs').children(':last-child').text()
-        const jiraTitle = $('#summary-val').text()
-        GM_setClipboard(`${jiraCode} ${jiraTitle}`, { type: 'text', mimetype: 'text/plain'})
-    })
+        $('#copy-simple').on('click', function(){
+            const jiraCode = $('.aui-nav.aui-nav-breadcrumbs').children(':last-child').text()
+            GM_setClipboard(jiraCode, { type: 'text', mimetype: 'text/plain'})
+        })
+        $('#copy-detailed').on('click', function(){
+            const jiraCode = $('.aui-nav.aui-nav-breadcrumbs').children(':last-child').text()
+            const jiraTitle = $('#summary-val').text()
+            GM_setClipboard(`${jiraCode} ${jiraTitle}`, { type: 'text', mimetype: 'text/plain'})
+        })
+    }
+
+    addJiraHelper()
+
+    const observer = new MutationObserver((mutationsList) => {
+        if (document.querySelector('#jira-helper')) return
+        addJiraHelper()
+    });
+    const container = document.querySelector('.issue-container') // old version
+    const container2 = document.querySelector('.issue-view')     // new version
+    observer.observe(container ? container : container2, { childList: true, subtree: true });
 })();
